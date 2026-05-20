5de7ec05c41ee3c6861b873aebbbf67470096820
DLDesigns <134949662+danthepilotman@users.noreply.github.com>
2026-05-20 19:11:25 -0400

    v1.0.14

--- src/sleep_timer.cpp
+++ src/sleep_timer.cpp
@@ -66,7 +66,7 @@ time_t nextTargetTime()  // Compute next scheduled target time
         time_t candidate = mktime(&tm_target);  // convert broken-out time to epoch candidate
 
         // If this candidate is still in the future, it's our next target
-        if ( difftime(candidate, now) >= 0 )
+        if ( candidate > now )
             return candidate;  // return first future schedule this day
         
     }
@@ -126,37 +126,13 @@ void deep_sleep_function()  // decide whether to sleep, wait, or continue runnin
 
 #endif
 
-        unsigned long startMillis = millis();
-
-        const unsigned long timeoutMs = ACTIVE_WINDOW_SEC * 1000UL;  // 61 seconds
-
-        while (true)
+     
+        while (difftime(target, time(nullptr)) > 0)
         {
-            now = time(nullptr);  // refresh current time
-
-            double remaining = difftime(target, now);
-
-            if (remaining <= 0)
-                break;  // target reached
-
-            // Timeout condition
-            if (millis() - startMillis >= timeoutMs)
-            {
-
-#ifdef DEBUG_ENABLED
-
-                DBGf("[WAIT] Timeout reached (61 seconds), breaking loop\r\n");
-#endif
-                break;
-            }
-
-#ifdef DEBUG_ENABLED
-
-            DBGf("[WAIT] %.0f seconds remaining\r\n", remaining);
-#endif
-
-            delay(950);
+            delay(200);
         }
+                
+
 
 #ifdef DEBUG_ENABLED
 
